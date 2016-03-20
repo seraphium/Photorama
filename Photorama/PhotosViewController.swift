a6555ae6db264545a3286428a473ffa3c0f8d128
Jackie Zhang <viperking@163.com>
2016-03-20 19:22:26 +0800

    first view created

---
+++ Photorama/PhotosViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PhotosViewController.swift
+//  Photorama
+//
+//  Created by Jackie Zhang on 16/3/20.
+//  Copyright © 2016年 Jackie Zhang. All rights reserved.
+//
+
+import UIKit
+
+
+class PhotosViewController : UIViewController {
+    
+    @IBOutlet var imageView : UIImageView!
+    
+}
